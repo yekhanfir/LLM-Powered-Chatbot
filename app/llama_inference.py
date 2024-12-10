caf2a80224d403160de49eb06bb8a56b8657363f
yessinek97 <y.khanfir@instadeep.com>
2024-12-10 22:46:39 +0100

    adapt generate response function

--- app/llama_inference.py
+++ app/llama_inference.py
@@ -14,6 +14,7 @@ new_model = "/mnt/c/Users/y.khanfir/llama_chatbot/Llama-3.2-1B/checkpoint-30"
 device_map = {"": 0}
 login(token="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")
 
+# load base model
 base_model = AutoModelForCausalLM.from_pretrained(
     model_name,
     low_cpu_mem_usage=True,
@@ -21,23 +22,31 @@ base_model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=torch.float16,
     device_map=device_map,
 )
+# load finetuned model from checkpoint
 model = PeftModel.from_pretrained(base_model, new_model)
+# merge peft weights from checkpoint into base model
 model = model.merge_and_unload()
 
+# initialize tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-# base_model, tokenizer = setup_chat_format(model, tokenizer)
 tokenizer.pad_token = tokenizer.eos_token
 
 
-
 def generate_response(prompt):
-    pipe = pipeline(task="text-generation", model=base_model, tokenizer=tokenizer, max_length=2000)
-    result = pipe(prompt)
+    """_summary_
 
+    Args:
+        prompt (_type_): _description_
 
-    pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=2000)
-    result = pipe(prompt)
-    return {'response': result[0]['generated_text']}
+    Returns:
+        _type_: _description_
+    """
+    prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    inputs = tokenizer(prompt, return_tensors='pt', padding=True, truncation=True).to("cuda")
+    outputs = model.generate(**inputs, max_new_tokens=150, num_return_sequences=1)
+    text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return {'response': text.split("assistant")[1]}
+
 
 if __name__ == '__main__':
     instruction = """You are a cute 21 years old girl that has feet fetich and likes to have 
